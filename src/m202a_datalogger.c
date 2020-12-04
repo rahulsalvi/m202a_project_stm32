de825cdfd7cc73fffadd92ea842ccc8d8e177238
Rahul Salvi <rsalvi006@me.com>
2020-12-03 23:39:46 -0800

    working reliable communication (i.e. start signal and crc works)

--- src/m202a_datalogger.c
+++ src/m202a_datalogger.c
@@ -12,6 +12,8 @@ CAN_FilterTypeDef filter;
 
 serial_msg_t ser_msg;
 
+#define DEBUG
+
 void setup() {
     init_serial_msg(&ser_msg);
 
@@ -33,6 +35,7 @@ void setup() {
 }
 
 void loop() {
+#ifndef DEBUG
     int msgs_available = HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_RX_FIFO0);
     while (msgs_available > 0) {
         ser_msg.num_msgs = 0;
@@ -42,11 +45,24 @@ void loop() {
             ser_msg.num_msgs++;
         }
         set_serial_crc(&hcrc, &ser_msg);
-        SEGGER_RTT_printf(0, "CRC: %d\n", ser_msg.crc32);
-        SEGGER_RTT_printf(0, "%d CAN msgs sent\n", ser_msg.num_msgs);
         HAL_UART_Transmit(&huart5, (uint8_t*)&ser_msg, sizeof(ser_msg), HAL_MAX_DELAY);
+        SEGGER_RTT_printf(0, "%d: %d CAN msgs sent\n", ser_msg.seq_number, ser_msg.num_msgs);
         ser_msg.seq_number++;
         msgs_available -= ser_msg.num_msgs;
     }
     HAL_Delay(10);
+#else
+    ser_msg.num_msgs = 4;
+    for (int i = 0; i < 4; i++) {
+        ser_msg.msgs[i].id = i;
+        for (int j = 0; j < 8; j++) {
+            ser_msg.msgs[i].data[j] = 0x55;
+        }
+    }
+    set_serial_crc(&hcrc, &ser_msg);
+    HAL_UART_Transmit(&huart5, (uint8_t*)&ser_msg, sizeof(ser_msg), HAL_MAX_DELAY);
+    SEGGER_RTT_printf(0, "%d: %d CAN msgs sent\n", ser_msg.seq_number, ser_msg.num_msgs);
+    ser_msg.seq_number++;
+    HAL_Delay(20);
+#endif
 }
--- src/serial_msgs.c
+++ src/serial_msgs.c
@@ -5,8 +5,21 @@ void init_serial_msg(serial_msg_t* m) {
     m->seq_number = 0;
 }
 
+void swap_endianness(uint32_t* src, uint32_t* dst) {
+    uint8_t* src8 = (uint8_t*)src;
+    uint8_t* dst8 = (uint8_t*)dst;
+    dst8[0] = src8[3];
+    dst8[1] = src8[2];
+    dst8[2] = src8[1];
+    dst8[3] = src8[0];
+}
+
 void set_serial_crc(CRC_HandleTypeDef* hcrc, serial_msg_t* m) {
-    uint32_t crc;
-    crc = HAL_CRC_Calculate(hcrc, (uint32_t*)m, offsetof(serial_msg_t, crc32));
+    const int crc_process_length = offsetof(serial_msg_t, crc32) / sizeof(uint32_t);
+    uint32_t buf[crc_process_length];
+    for (int i = 0; i < crc_process_length; i++) {
+        swap_endianness(&((uint32_t*)m)[i], &buf[i]);
+    }
+    uint32_t crc = HAL_CRC_Calculate(hcrc, buf, crc_process_length);
     m->crc32 = crc;
 }
